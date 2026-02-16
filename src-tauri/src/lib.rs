dd5e5689dffa28f25e70e51878229704c4e3af6e
ni00 <mail@nixiaobai.com>
2026-02-16 20:56:30 +0800

    feat: implement a splash screen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{Listener, Manager};
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -6,6 +8,20 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_safe_area_insets_css::init())
+        .setup(|app| {
+            let app_handle = app.handle().clone();
+
+            app.listen("frontend-ready", move |_| {
+                if let Some(splashscreen) = app_handle.get_webview_window("splashscreen") {
+                    let _ = splashscreen.close();
+                }
+                if let Some(main_window) = app_handle.get_webview_window("main") {
+                    let _ = main_window.show();
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react'
+import { emit } from '@tauri-apps/api/event'
+
 import './global.css'
 
 import AppRouter from '@/app/router'
 import AppProvider from '@/app/provider'
 
 export default function App() {
+    useEffect(() => {
+        emit('frontend-ready')
+    }, [])
+
     return (
         <AppProvider>
             <AppRouter />
