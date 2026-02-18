2185f70c6cff1c6056ea5a68d76bbdc070eaae2a
ni00 <mail@nixiaobai.com>
2026-02-18 21:38:21 +0800

    fix: resolve linux appImage rendering issues

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    if std::env::var("APPIMAGE").is_ok() {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
     nillm::run();
 }
